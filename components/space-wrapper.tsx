7b17e1d0a7bb41ba5c7abf44f0824423eb6c0205
Robert-Lexicon <robert.hedblad@lexicon.se>
2025-08-12 15:27:10 +0200

    Removed old components

--- components/space-wrapper.tsx
+++
@@ -1,9 +0,0 @@
-export default function SpaceWrapper({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  // This component is used to wrap content with a consistent padding
-  // https://react.dev/learn/passing-props-to-a-component#passing-jsx-as-children
-  return <div className="px-4 md:px-8">{children}</div>;
-}
