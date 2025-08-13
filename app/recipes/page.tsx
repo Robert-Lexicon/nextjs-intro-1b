f5c78d0210668ee9ed17f8d887ce04f0040fad37
Robert-Lexicon <robert.hedblad@lexicon.se>
2025-08-13 11:52:45 +0200

    Example for Static Metadata and supsense

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,7 +1,9 @@
 import Card from "@/components/card";
 import MainWrapper from "@/components/main-wrapper";
 import { Recipe } from "@/lib/interfaces";
+import { Metadata } from "next";
 import Link from "next/link";
+import { Suspense } from "react";
 //import data from "@/lib/recipes.json";
 
 interface RecipeResponse {
@@ -10,6 +12,11 @@ interface RecipeResponse {
   skip: number;
   total: number;
 }
+
+export const metadata: Metadata = {
+  title: "Our Recipes",
+  description: "A collection of delicious recipes",
+};
 
 export default async function Recipes() {
   const response = await fetch("https://dummyjson.com/recipes");
@@ -22,18 +29,21 @@ export default async function Recipes() {
   //const recipes: Recipe[] = data["recipes"];
   return (
     <MainWrapper title="Recipes">
-      <ul className="grid gap-4 grid-cols-[repeat(auto-fit,minmax(30ch,1fr))] content-stretch">
-        {recipes.map((recipe, i) => (
-          <li key={i}>
-            <Link href={`/recipes/${recipe.id}`}>
-              <Card
-                recipe={recipe}
-                className="border border-neutral-300 rounded p-4 shadow-sm"
-              />
-            </Link>
-          </li>
-        ))}
-      </ul>
+      {/* https://nextjs.org/docs/app/getting-started/fetching-data#with-suspense */}
+      <Suspense fallback={<p>Loading recipes...</p>}>
+        <ul className="grid gap-4 grid-cols-[repeat(auto-fit,minmax(30ch,1fr))] content-stretch">
+          {recipes.map((recipe, i) => (
+            <li key={i}>
+              <Link href={`/recipes/${recipe.id}`}>
+                <Card
+                  recipe={recipe}
+                  className="border border-neutral-300 rounded p-4 shadow-sm"
+                />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </Suspense>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque dicta
         culpa veniam iusto voluptate laboriosam itaque, ullam, exercitationem
