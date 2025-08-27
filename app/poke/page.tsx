ec4e9ccc87e88abbb9ee84b1a878f57f718b1736
Robert-Lexicon <robert.hedblad@lexicon.se>
2025-08-27 12:02:06 +0200

    added pokemon search page example on pokemon route using promise.all

--- app/poke/page.tsx
+++ app/poke/page.tsx
@@ -1,32 +1,75 @@
-interface PokeToRender {
-  img: string;
+import Search from "@/components/search";
+import SearchWButton from "@/components/search-w-button";
+
+interface PokemonShort {
   name: string;
-  order: number;
+  url: string;
 }
-export default async function Poke() {
-  const secondResponse = await fetch("https://pokeapi.co/api/v2/pokemon/1");
 
-  const detailedData = await secondResponse.json();
-  console.log(detailedData);
+interface PokemonFull {
+  name: string;
+  height: number;
+  id: number;
+}
 
-  //här destructurerar vi ut det vi vill ha, i detta fall img, name och order
-  const {
-    sprites: {
-      other: {
-        "official-artwork": { front_default: img },
-      },
-    },
-    name,
-    id: order,
-  } = detailedData;
+async function fetchAllPokemonData(
+  list: PokemonShort[]
+): Promise<PokemonFull[]> {
+  const fetchPromises = list.map((item) => fetch(item.url));
 
-  //här gör vi ett nytt objekt
-  const pokemon: PokeToRender = {
-    img,
-    name,
-    order,
-  };
-  console.log(pokemon);
+  try {
+    const responses = await Promise.all(fetchPromises);
 
-  return <div>Hej</div>;
+    if (responses.some((res) => !res.ok)) {
+      throw new Error("one or more requests failed");
+    }
+
+    const jsonPromises = responses.map((res) => res.json());
+    return await Promise.all(jsonPromises);
+  } catch (e) {
+    console.error("sjdflskjd");
+    return [];
+  }
+}
+
+export default async function Poke({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | undefined }>;
+}) {
+  const { query = "" } = await searchParams;
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+
+  const { results }: { results: PokemonShort[] } = await response.json();
+  // console.log(results);
+
+  //find first only
+  const singlePokemon = results.find((p) => p.name.includes(query));
+  //console.log(singlePokemon);
+
+  //find all
+  const allPokemons = results.filter((p) => p.name.includes(query));
+  //console.log(allPokemons);
+
+  const allPokemonData =
+    query !== "" ? await fetchAllPokemonData(allPokemons) : [];
+
+  console.log(allPokemonData);
+
+  return (
+    <div>
+      <Search placeholder="Search for pokemon" />
+      <SearchWButton />
+      {allPokemons.map((p) => (
+        <a key={p.name} href={"/poke/" + p.name}>
+          {p.name}
+        </a>
+      ))}
+      {allPokemonData.map((p) => (
+        <a key={p.name} href={"/poke/" + p.name}>
+          {p.name}
+        </a>
+      ))}
+    </div>
+  );
 }
