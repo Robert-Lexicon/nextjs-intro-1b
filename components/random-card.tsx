780135730b8453b5da69419517adbff64257972f
Robert-Lexicon <robert.hedblad@lexicon.se>
2025-09-01 13:20:31 +0200

    Added tag-color example

--- components/random-card.tsx
+++ components/random-card.tsx
@@ -1,3 +1,4 @@
+import { tagColors } from "@/lib/data/tag-colors";
 import Image from "next/image";
 
 interface PokemonFull {
@@ -5,6 +6,7 @@ interface PokemonFull {
   weight: number;
   height: number;
   id: number;
+  types: Array<{ type: { name: string } }>;
   sprites: {
     other: {
       "official-artwork": {
@@ -29,17 +31,49 @@ export default async function PokemonCard({ id }: { id?: number }) {
     return null;
   }
 
+  //get the first type since this is our main type
+  // as keyof typeof is to add the right key type in tagColors
+  const mainType = randomPokemon.types[0].type.name as keyof typeof tagColors;
+  const borderClass = tagColors[mainType].border;
+  const badgeClass = tagColors[mainType].badge;
+
   return (
-    <div className="border rounded border-neutral-400 p-4">
-      <h3 className="text-lg font-bold text-center capitalize">
+    <div className="grid gap-2 justify-items-center bg-teal-50 dark:bg-slate-900 rounded-lg shadow-md border-indigo-400 border-6 py-16 px-4">
+      <h3 className="font-jersey text-3xl text-center capitalize pb-1.5">
         {randomPokemon.name}
       </h3>
-      <Image
-        alt="random"
-        height={200}
-        width={200}
-        src={randomPokemon.sprites.other["official-artwork"].front_default}
-      />
+      <span
+        className={`-order-1 rounded-full px-2 py-1 flex items-center justify-center text-xs font-bold capitalize text-neutral-50 ${badgeClass}`}
+      >
+        #{randomPokemon.id}
+      </span>
+      <div
+        className={`order-first rounded-full w-2/3 overflow-clip aspect-square border-3 flex items-center justify-center bg-neutral-100 dark:bg-neutral-900 ${borderClass}`}
+      >
+        <Image
+          className="p-4"
+          alt="random"
+          height={200}
+          width={200}
+          src={randomPokemon.sprites.other["official-artwork"].front_default}
+        />
+      </div>
+
+      <ul className="flex flex-wrap gap-2">
+        {randomPokemon.types.map(({ type }, i) => {
+          //here we get the tagcolor for the current type and then return our object
+          //if we want to do ts/js inside a map we can do this by adding { ... return <></>} to the .map
+          const bgClass = tagColors[type.name as keyof typeof tagColors].badge;
+          return (
+            <li
+              className={`rounded-full px-3 py-1 flex items-center justify-center text-sm font-bold capitalize text-neutral-100  ${bgClass}`}
+              key={i}
+            >
+              {type.name}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
---
+++ lib/data/tag-colors.ts
@@ -0,0 +1,74 @@
+export const tagColors = {
+  normal: {
+    border: "border-gray-200",
+    badge: "bg-gray-500",
+  },
+  fire: {
+    border: "border-amber-400",
+    badge: "bg-amber-600",
+  },
+  water: {
+    border: "border-blue-400",
+    badge: "bg-blue-600",
+  },
+  electric: {
+    border: "border-yellow-200",
+    badge: "bg-yellow-600",
+  },
+  grass: {
+    border: "border-green-400",
+    badge: "bg-green-700",
+  },
+  ice: {
+    border: "border-cyan-100",
+    badge: "bg-cyan-400",
+  },
+  fighting: {
+    border: "border-red-500",
+    badge: "bg-red-500",
+  },
+  poison: {
+    border: "border-purple-500",
+    badge: "bg-purple-500",
+  },
+  ground: {
+    border: "border-yellow-500",
+    badge: "bg-yellow-500",
+  },
+  flying: {
+    border: "border-indigo-200",
+    badge: "bg-indigo-400",
+  },
+  psychic: {
+    border: "border-pink-300",
+    badge: "bg-pink-400",
+  },
+  bug: {
+    border: "border-lime-400",
+    badge: "bg-lime-700",
+  },
+  rock: {
+    border: "border-stone-400",
+    badge: "bg-stone-500",
+  },
+  ghost: {
+    border: "border-indigo-600",
+    badge: "bg-indigo-700",
+  },
+  dragon: {
+    border: "border-indigo-400",
+    badge: "bg-indigo-500",
+  },
+  dark: {
+    border: "border-gray-600",
+    badge: "bg-gray-800",
+  },
+  steel: {
+    border: "border-slate-200",
+    badge: "bg-slate-500",
+  },
+  fairy: {
+    border: "border-pink-100",
+    badge: "bg-pink-400",
+  },
+};
